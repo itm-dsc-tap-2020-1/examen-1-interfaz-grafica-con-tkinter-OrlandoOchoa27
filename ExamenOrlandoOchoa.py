50817f3106e8139c8b68b529cfeb4e119a508194
OrlandoOchoa27 <61324036+OrlandoOchoa27@users.noreply.github.com>
2020-02-26 19:45:44 +0100

    Add files via upload

--- ExamenOrlandoOchoa.py
+++ ExamenOrlandoOchoa.py
@@ -14,8 +14,7 @@ tab1=ttk.Frame(tabControl)
 tabControl.add(tab1,text='')
 tabControl.pack(expand=1,fill="both")
 
-def fun2():
-    i=i+1
+
 preg1=ttk.Label(tab1,text='Menciona las razones de la caida del imperio Romano:')
 preg1.grid(column=0,row=0)
 pregu1=tk.StringVar()
@@ -51,21 +50,21 @@ radio5=ttk.Radiobutton(tab1,text='Platon',variable=opcion5,value="Platon")
 radio5.grid(column=1,row=6,sticky=tk.W)
 radio6=ttk.Radiobutton(tab1,text='Edipo',variable=opcion5,value="Edipo")
 radio6.grid(column=2,row=6,sticky=tk.W)
-
+opcion10=tk.IntVar()
 preg5=ttk.Label(tab1,text='Dios(es) de la mitologia griega mas poderoso(s)')
 preg5.grid(column=0,row=8)
-cb1=ttk.Checkbutton(tab1,text='Zeus',variable="Zeus")
-cb2=ttk.Checkbutton(tab1,text='Morfeo',variable="Morfeo")
-cb3=ttk.Checkbutton(tab1,text='Hades',variable="Hades")
-cb4=ttk.Checkbutton(tab1,text='Athenea',variable="Athenea")
-cb5=ttk.Checkbutton(tab1,text='Afrodita',variable="Afrodita")
+cb1=ttk.Checkbutton(tab1,text='Zeus',variable=opcion6)
+cb2=ttk.Checkbutton(tab1,text='Morfeo',variable=opcion7)
+cb3=ttk.Checkbutton(tab1,text='Hades',variable=opcion8)
+cb4=ttk.Checkbutton(tab1,text='Athenea',variable=opcion9)
+cb5=ttk.Checkbutton(tab1,text='Afrodita',variable=opcion10)
 
 cb1.grid(column=0,row=9)
 cb2.grid(column=1,row=9)
 cb3.grid(column=2,row=9)
 cb4.grid(column=3,row=9)
 cb5.grid(column=4,row=9)
-i=0
+
 def fun4():
     ventana.quit()
     ventana.destroy()
@@ -74,25 +73,25 @@ def fun4():
 
 
 def fun6():
-    calif=ttk.Label(tab1,text='Felicidades sacaste 100')
-    calif.grid(column=2,row=10)
+    i=2
 
-    cadena="RESPUESTAS\n"
-    cadena+="\n"+pregu1.get()
-    cadena+="\n"+pregu2.get()
-    cadena+="\n"+radio1.get()
-    cadena+="\n"+opcion4.get()
-    cadena+="\n"+opcion5.get()
+    if(opcion4=="Athenea"):
+        i=i+1
+    if(opcion5=="Edipo"):
+        i=i+1
+    if(opcion6.get()!=0 and opcion8.get()!=0):
+      i=i+1
+    sum=i*20
+    mBox.showinfo('Tu Calificacion =',sum)
 
-    mBox.showinfo('Felicidades sacaste 100',cadena)
-
+   
     
     
 
     
 
     
-    mBox.showinfo('Respuestas',cadena)
+  
 
 cf=ttk.Button(tab1,text='Calificar',command=fun6)
 cf.grid(column=4,row=11)
